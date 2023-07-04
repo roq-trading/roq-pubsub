6b16569d9ebbbc965942ed3c4f17425d2d955798
thraneh <thraneh@gmail.com>
2023-07-04 14:35:16 +0200

    refactor logging library (#356)

--- src/roq/pubsub/main.cpp
+++ src/roq/pubsub/main.cpp
@@ -2,6 +2,9 @@
 
 #include "roq/pubsub/application.hpp"
 
+#include "roq/logging/flags/parser.hpp"
+#include "roq/logging/flags/settings.hpp"
+
 using namespace std::literals;
 
 // === CONSTANTS ===
@@ -21,5 +24,7 @@ auto const INFO = roq::Service::Info{
 // === IMPLEMENTATION ===
 
 int main(int argc, char **argv) {
-  return roq::pubsub::Application{argc, argv, INFO}.run();
+  roq::logging::flags::Parser parser{argc, argv, INFO.description, INFO.build_version};
+  auto settings = roq::logging::flags::create_settings();
+  return roq::pubsub::Application{parser, settings, INFO}.run();
 }
