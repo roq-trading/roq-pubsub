0e6d87673f9fac9ef157be102805cc25fbc545dd
thraneh <thraneh@gmail.com>
2024-01-10 04:38:33 +0100

    upgrade format_parse_context and format_context

--- src/roq/pubsub/config.hpp
+++ src/roq/pubsub/config.hpp
@@ -61,12 +61,8 @@ struct Config final : public server::config::Dispatcher, public server::config::
 
 template <>
 struct fmt::formatter<roq::pubsub::Config> {
-  template <typename Context>
-  constexpr auto parse(Context &context) {
-    return std::begin(context);
-  }
-  template <typename Context>
-  auto format(roq::pubsub::Config const &value, Context &context) const {
+  constexpr auto parse(format_parse_context &context) { return std::begin(context); }
+  auto format(roq::pubsub::Config const &value, format_context &context) const {
     using namespace std::literals;
     return fmt::format_to(
         context.out(),
