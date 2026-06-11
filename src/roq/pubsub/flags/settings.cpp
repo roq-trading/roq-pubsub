6c9f8cab7fb9295061c7a79ce367da32ab07aa63
thraneh <thraneh@gmail.com>
2026-06-11 06:34:08 +0000

    send git-hash to license server

--- src/roq/pubsub/flags/settings.cpp
+++ src/roq/pubsub/flags/settings.cpp
@@ -10,7 +10,8 @@ namespace roq {
 namespace pubsub {
 namespace flags {
 
-Settings::Settings(args::Parser const &args) : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER}, flags::Flags{flags::Flags::create()} {
+Settings::Settings(args::Parser const &args)
+    : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER, ROQ_GIT_DESCRIBE_HASH, {}}, flags::Flags{flags::Flags::create()} {
   log::info("settings={}"sv, *this);
 }
 
